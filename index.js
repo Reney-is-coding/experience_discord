6a86a087030eac1334ceff95f48feb5436169f75
antilope <benjamin.grout@epitech.eu>
2020-08-20 16:21:47 +0200

    a

--- index.js
+++ index.js
@@ -9,10 +9,14 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-  if (message.content === '!ping') {
+  if (message.content === prefix + 'ping') {
   	// send back "Pong." to the channel the message was sent in
   	message.channel.send('Pong.');
   }
+
+	if (message.content === prefix + 'help') {
+		message.channel.send('Commandes disponibles : !ping, ! help')
+	}
 });
 
 client.login(token);
